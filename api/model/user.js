8dda15e547b0601f55940726c0eb61902ccda9c9
tevonial <tevancollins@gmail.com>
2017-06-03 15:40:58 -0600

    Remove old code.

--- api/model/user.js
+++ api/model/user.js
@@ -44,7 +44,7 @@ userSchema.methods.generateJwt = function() {
         _id: this._id,
         email: this.email,
         name: this.name,
-        exp: parseInt(expiry.getTime() / 1000),
+        exp: parseInt(expiry.getTime() / 1000)
     }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
 };
 
