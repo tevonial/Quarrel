69126d5cf51d1c8b7d15e6a82cb732df7b3c5f75
tevonial <tevancollins@gmail.com>
2017-06-03 15:40:58 -0600

    Remove old code.

--- api/model/index.js
+++ api/model/index.js
@@ -7,8 +7,8 @@
 // Database configuration
 //================================================================================
 var mongoose = require('mongoose');
-
 var db = 'mongodb://admin:pass@ds137141.mlab.com:37141/tevonial-mean';
+// var db = 'mongodb://127.0.0.1:27017/quarrel';
 mongoose.connect(db);
 
 
